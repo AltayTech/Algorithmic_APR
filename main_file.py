aa26ab02efd2ef41032234178e851b439cc72771
riza_fernus <mojed.reza@gmail.com>
2024-05-16 11:59:50 +0300

    Implemented - whole book from  image input

--- main_file.py
+++ main_file.py
@@ -15,8 +15,8 @@ from PIL import Image, ImageDraw
 pdf_path = 'assets\\p12.pdf'
 image_path = 'assets\\4\\p-210.png'
 # config parameter
-number_of_option = 5
-number_of_column = 2
+number_of_option = 4
+number_of_column = 0
 question_margin_top = 5
 question_margin_bottom = 5
 question_margin_left = 5
@@ -151,20 +151,20 @@ def extract_question(pdf_path, page_number, book_number):
     print(f"page_number: {page_number}")
 
     # PDF'ten metin içeriği ve metinlerin koordinatlarını çıkar
-    with pdfplumber.open(pdf_path, ) as pdf:
-        detected_question_numbers_general = {}
-        detect_question = {}
-        all_boxes = []
-        detected_options = {}
-        detected_options_box = []
+    detected_question_numbers_general = {}
+    detect_question = {}
+    all_boxes = []
+    detected_options = {}
+    detected_options_box = []
 
-        if input_method == 'image':
-            image = cv2.imread(image_path, )
-            image_height, image_width, _ = image.shape
-            extracted_texts_with_coords = extract_text_with_coords(image_path)
-            # print(f"extracted_texts_with_coords: {extracted_texts_with_coords}")
+    if input_method == 'image':
+        image = cv2.imread(image_path, )
+        image_height, image_width, _ = image.shape
+        extracted_texts_with_coords = extract_text_with_coords(image_path)
+        # print(f"extracted_texts_with_coords: {extracted_texts_with_coords}")
 
-        else:
+    else:
+        with pdfplumber.open(pdf_path, ) as pdf:
 
             # pdf
             image_width = pdf.pages[page_number].width
@@ -178,112 +178,112 @@ def extract_question(pdf_path, page_number, book_number):
             extracted_texts_with_coords = page.extract_words()
             # print(f"extracted_texts_with_coords: {extracted_texts_with_coords}")
 
-        for item in extracted_texts_with_coords:
-            # print(f"item index: {item}")
-            # print(f"item color: {item.get('stroking_color')}")
-            detected_options = {}
+    for item in extracted_texts_with_coords:
+        # print(f"item index: {item}")
+        # print(f"item color: {item.get('stroking_color')}")
+        detected_options = {}
 
-            text = item['text'].strip()  # Boşlukları temizleme
-            x0, y0, x1, y1 = float(item['x0']), float(item['top']), float(item['x1']), float(item['bottom'])
-            all_boxes.append((x0, y0, x1, y1))
+        text = item['text'].strip()  # Boşlukları temizleme
+        x0, y0, x1, y1 = float(item['x0']), float(item['top']), float(item['x1']), float(item['bottom'])
+        all_boxes.append((x0, y0, x1, y1))
 
-            if re.match(r"^[1-9][0-9]{0,2}\.$", text) and 1 <= int(text[:-1]) <= 120:
-                # print(f"item index: {text}")
+        if re.match(r"^[1-9][0-9]{0,2}\.$", text) and 1 <= int(text[:-1]) <= 120:
+            # print(f"item index: {text}")
 
-                detected_question_numbers_general[text] = (x0, y0, x1, y1)
-                print(f"item detected_question_numbers_general: {detected_question_numbers_general}")
+            detected_question_numbers_general[text] = (x0, y0, x1, y1)
+            print(f"item detected_question_numbers_general: {detected_question_numbers_general}")
 
-            # A, B, C, D, E seçeneklerini tespit etme
-            if re.match(r"^\s?[ABCDE]\s?\)$", text):
-                detected_options[text] = (x0, y0, x1, y1)
-                # print(f"detected_options: {detected_options}")
-                detected_options_box.append(detected_options)
+        # A, B, C, D, E seçeneklerini tespit etme
+        if re.match(r"^\s?[ABCDE]\s?\)$", text):
+            detected_options[text] = (x0, y0, x1, y1)
+            # print(f"detected_options: {detected_options}")
+            detected_options_box.append(detected_options)
 
-        # Determine the last option item(D orE)
+    # Determine the last option item(D orE)
 
-        if number_of_option == 4:
-            last_option = 'D)'
-        else:
-            last_option = 'E)'
+    if number_of_option == 4:
+        last_option = 'D)'
+    else:
+        last_option = 'E)'
 
-        # Determine the last option item(D or E)
-        for item in detected_question_numbers_general:
+    # Determine the last option item(D or E)
+    for item in detected_question_numbers_general:
 
-            x0, y0, x1, y1 = (detected_question_numbers_general[item][0], detected_question_numbers_general[item][1],
+        x0, y0, x1, y1 = (detected_question_numbers_general[item][0], detected_question_numbers_general[item][1],
 
-                              image_width,
-                              image_height
-                              )
-            detect_question[item] = (x0, y0, x1, y1)
-            nextHorizontalQuestionX = x1
-            nexVerticallyQuestionY = y1
+                          image_width,
+                          image_height
+                          )
+        detect_question[item] = (x0, y0, x1, y1)
+        nextHorizontalQuestionX = x1
+        nexVerticallyQuestionY = y1
 
-            for item2 in detected_question_numbers_general:
-                if is_in_right(detect_question[item],
-                               detected_question_numbers_general[item2]) and number_of_column == 2:
-                    nextHorizontalQuestionX = detected_question_numbers_general[item2][0] - 3
-                    x0, y0, x1, y1 = (detect_question[item][0], detect_question[item][1],
-                                      image_width,
-                                      image_height)
-                    detect_question[item] = (x0, y0, x1, y1)
+        for item2 in detected_question_numbers_general:
+            if is_in_right(detect_question[item],
+                           detected_question_numbers_general[item2]) and number_of_column == 2:
+                nextHorizontalQuestionX = detected_question_numbers_general[item2][0] - 3
+                x0, y0, x1, y1 = (detect_question[item][0], detect_question[item][1],
+                                  image_width,
+                                  image_height)
+                detect_question[item] = (x0, y0, x1, y1)
 
-                if is_in_the_region(detect_question[item], detected_question_numbers_general[item2]):
-                    # print(f"is_in_the_region: {detected_question_numbers_general[item2]}")
+            if is_in_the_region(detect_question[item], detected_question_numbers_general[item2]):
+                # print(f"is_in_the_region: {detected_question_numbers_general[item2]}")
 
-                    detect_question[item] = (x0, y0, x1, y1)
-                    if not is_in_right(detect_question[item], detected_question_numbers_general[item2]):
-                        if is_in_blew(detect_question[item], detected_question_numbers_general[item2]):
-                            # print(f"is_in_blew: {detected_question_numbers_general[item2]}")
-                            nexVerticallyQuestionY = detected_question_numbers_general[item2][1] - 3
-                            x0, y0, x1, y1 = (detect_question[item][0], detect_question[item][1],
-                                              nextHorizontalQuestionX,
-                                              nexVerticallyQuestionY)
-                            detect_question[item] = (x0, y0, x1, y1)
-                    else:
-                        nextHorizontalQuestionX = detected_question_numbers_general[item2][0] - 3
+                detect_question[item] = (x0, y0, x1, y1)
+                if not is_in_right(detect_question[item], detected_question_numbers_general[item2]):
+                    if is_in_blew(detect_question[item], detected_question_numbers_general[item2]):
+                        # print(f"is_in_blew: {detected_question_numbers_general[item2]}")
+                        nexVerticallyQuestionY = detected_question_numbers_general[item2][1] - 3
                         x0, y0, x1, y1 = (detect_question[item][0], detect_question[item][1],
                                           nextHorizontalQuestionX,
                                           nexVerticallyQuestionY)
                         detect_question[item] = (x0, y0, x1, y1)
-            number_of_included_option = 0
-            have_last_option = False
-            for detected_options_item in detected_options_box:
-                for option_item in detected_options_item:
-                    if is_in_the_region(detect_question[item], detected_options_item[option_item]):
-                        number_of_included_option = number_of_included_option + 1
-                        nexVerticallyQuestionY = detected_options_item[option_item][3]
-
-                        if option_item == last_option:
-                            nexVerticallyQuestionY = detected_options_item[option_item][3]
-            x0, y0, x1, y1 = (detect_question[item][0], detect_question[item][1],
-                              nextHorizontalQuestionX,
-                              nexVerticallyQuestionY)
-            detect_question[item] = (x0, y0, x1, y1)
-
-            if number_of_included_option < 1:
-                del detect_question[item]
-            else:
-                dis_item = 0
-                selected_item = detect_question[item]
-                for all_boxes_item in all_boxes:
-
-                    if is_in_the_region(detect_question[item], all_boxes_item):
-                        if dis_item < all_boxes_item[2] - detect_question[item][0]:
-                            dis_item = all_boxes_item[2] - detect_question[item][0]
-                            selected_item = all_boxes_item
-                    nextHorizontalQuestionX = selected_item[2]
+                else:
+                    nextHorizontalQuestionX = detected_question_numbers_general[item2][0] - 3
                     x0, y0, x1, y1 = (detect_question[item][0], detect_question[item][1],
                                       nextHorizontalQuestionX,
                                       nexVerticallyQuestionY)
-                detect_question[item] = (x0, y0, x1, y1)
+                    detect_question[item] = (x0, y0, x1, y1)
+        number_of_included_option = 0
+        have_last_option = False
+        for detected_options_item in detected_options_box:
+            for option_item in detected_options_item:
+                if is_in_the_region(detect_question[item], detected_options_item[option_item]):
+                    number_of_included_option = number_of_included_option + 1
+                    nexVerticallyQuestionY = detected_options_item[option_item][3]
 
-                x0, y0, x1, y1 = (
-                    detect_question[item][0] - question_margin_left, detect_question[item][1] - question_margin_top,
-                    nextHorizontalQuestionX + question_margin_right,
-                    nexVerticallyQuestionY + question_margin_bottom)
-                detect_question[item] = (x0, y0, x1, y1)
+                    if option_item == last_option:
+                        nexVerticallyQuestionY = detected_options_item[option_item][3]
+        x0, y0, x1, y1 = (detect_question[item][0], detect_question[item][1],
+                          nextHorizontalQuestionX,
+                          nexVerticallyQuestionY)
+        detect_question[item] = (x0, y0, x1, y1)
 
-    print(f"detected_options_box: {len(detected_options_box)}")
+        if number_of_included_option < 1:
+            del detect_question[item]
+        else:
+            dis_item = 0
+            selected_item = detect_question[item]
+            for all_boxes_item in all_boxes:
+
+                if is_in_the_region(detect_question[item], all_boxes_item):
+                    if dis_item < all_boxes_item[2] - detect_question[item][0]:
+                        dis_item = all_boxes_item[2] - detect_question[item][0]
+                        selected_item = all_boxes_item
+                nextHorizontalQuestionX = selected_item[2]
+                x0, y0, x1, y1 = (detect_question[item][0], detect_question[item][1],
+                                  nextHorizontalQuestionX,
+                                  nexVerticallyQuestionY)
+            detect_question[item] = (x0, y0, x1, y1)
+
+            x0, y0, x1, y1 = (
+                detect_question[item][0] - question_margin_left, detect_question[item][1] - question_margin_top,
+                nextHorizontalQuestionX + question_margin_right,
+                nexVerticallyQuestionY + question_margin_bottom)
+            detect_question[item] = (x0, y0, x1, y1)
+
+    print(f"detect_question: {len(detect_question)}")
 
     if input_method == 'pdf':
         # print(f"input_method: {input_method}")
@@ -327,16 +327,25 @@ def extract_question(pdf_path, page_number, book_number):
 start_time = time.time()
 
 # extract_question(17)
-for j in range(5, 12, 1):
-    pdf_path = f'assets\\p{j}.pdf'
-    os.makedirs(f"results{j}", exist_ok=True)
-    book_number = j
+if input_method == 'pdf':
+    for j in range(10, 11, 1):
+        pdf_path = f'assets\\p{j}.pdf'
+        os.makedirs(f"results{j}", exist_ok=True)
+        book_number = j
+        print(f"book_number: {book_number}")
+
     with pdfplumber.open(pdf_path, ) as pdf:
         total_page = len(pdf.pages)
         print(f"page number book p{j}: {total_page}")
 
-    for i in range(1, total_page - 1, 1):
+    for i in range(150, 179, 1):
         extract_question(pdf_path, i, book_number)
+else:
+    for i in range(1, 52, 1):
+        image_path = f'assets\\4\\p-{i}.png'
+
+        extract_question(image_path, i, 5)
+
 end_time = time.time()
 execution_time = end_time - start_time
 print(f"execution_time {execution_time:.2f}")
